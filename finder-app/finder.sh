10816664db5e617c6f5ec9e29c29ffbb50102f07
Viswesh <viswesh@Ubuntu.myguest.virtualbox.org>
2023-09-04 17:49:06 -0600

    *final*

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# Checking for number of arguments
 
+# Author: Visweshwaran Baskaran
+# Script name: finder.sh
+# Description: 
+# Shell script to take 2 arguments from user and find no. of files in that directory and subdirectory (path is first argument), and no. of occurences of string in same path (string is second argument)
+# References:
+# 	1) https://www.geeksforgeeks.org/how-to-count-files-in-directory-recursively-in-linux/
+#	2) https://stackoverflow.com/questions/4121803/how-can-i-use-grep-to-find-a-wordinside-a-folder
+
+# Checking for number of arguments
 if [ $# -eq 2 ]
 then
 	echo "Valid Number of Arguments"
@@ -10,6 +18,7 @@ else
 	exit 1
 fi
 
+# Checking if the file directory exists
 if [ -d "$1" ]
 then
 	echo "File directory $1 exists "
@@ -18,11 +27,12 @@ else
 	exit 1
 fi
 
-#refered https://www.geeksforgeeks.org/how-to-count-files-in-directory-recursively-in-linux/
-no_files=$(find $1 -type f | wc -l)
-#refered https://stackoverflow.com/questions/4121803/how-can-i-use-grep-to-find-a-word-inside-a-folder
-no_lines=$(grep -r $2 $1 | wc -l)
+# Storing arguments in variables after validating
+filesdir=$1
+searchstr=$2
+ 
+no_files=$(find $filesdir -type f | wc -l)
+no_lines=$(grep -r $searchstr $filesdir | wc -l)
 
 echo "The number of files are $no_files and the number of matching lines are $no_lines"
-
 exit 0
